c248762789921ae362c0443ad1c1604ffe895f3b
gabrielewanzeler <gabrielewanzeler000@gmail.com>
2024-07-08 07:04:55 -0300

    up

--- script.js
+++ script.js
@@ -16,3 +16,24 @@ document.addEventListener('click', function(event) {
         }
     }
 });
+
+// Função para alternar o menu (sidebar) (abrir/fechar)
+function alternarSidebar2() {
+    var sidebar2 = document.querySelector('.sidebar2');
+    sidebar2.classList.toggle('active');
+}
+
+// Adiciona evento de clique ao botão do menu
+document.querySelector('.menu-button2').addEventListener('click', alternarSidebar2);
+
+// Fecha o sidebar se clicar fora dele ou do botão
+document.addEventListener('click', function(event) {
+    var sidebar2 = document.querySelector('.sidebar2');
+    var menuButton2 = document.querySelector('.menu-button2');
+    
+    if (!sidebar2.contains(event.target) && !menuButton2.contains(event.target)) {
+        if (sidebar2.classList.contains('active')) {
+            alternarSidebar2();
+        }
+    }
+});
